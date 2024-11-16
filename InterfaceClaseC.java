b57649aacae60217480ec6a7faca1caf2ccf2bd0
Adriana Martínez Fuentes <mar24086@uvg.edu.gt>
2024-11-15 20:25:30 -0600

    Clase C

---
+++ InterfaceClaseC.java
@@ -0,0 +1,6 @@
+/**
+ * Interface específica para radios Clase C, extendiendo funcionalidades básicas.
+ */
+public interface InterfaceClaseC extends InterfaceRadio {
+    void cambiarModo(String nuevoModo);
+}
