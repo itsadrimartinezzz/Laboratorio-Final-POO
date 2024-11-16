1f0ab655fba24fd836ec327ad900017fb1066266
Adriana Martínez Fuentes <mar24086@uvg.edu.gt>
2024-11-15 21:52:49 -0600

    Actualización Interface Clase C

--- InterfaceClaseC.java
+++ InterfaceClaseC.java
@@ -1,6 +1,9 @@
-/**
- * Interface específica para radios Clase C, extendiendo funcionalidades básicas.
- */
+
 public interface InterfaceClaseC extends InterfaceRadio {
+    /**
+     * Cambia el modo actual del radio a uno nuevo.
+     * 
+     * @param nuevoModo El nuevo modo que se desea establecer (Radio, Reproducción, Teléfono, etc.).
+     */
     void cambiarModo(String nuevoModo);
 }
