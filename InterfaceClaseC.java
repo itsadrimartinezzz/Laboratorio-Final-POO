53cebe3f025364d514cf67c0be208d6d3a19a856
Adriana Martínez Fuentes <mar24086@uvg.edu.gt>
2024-11-15 22:01:32 -0600

    Encabezado InterfaceClaseC

--- InterfaceClaseC.java
+++ InterfaceClaseC.java
@@ -1,3 +1,17 @@
+/**
+ * Universidad del Valle de Guatemala - Segundo Semestre 2024
+ * Programación Orientada a Objetos (POO)
+ * Laboratorio Final
+ * 
+ * Interface InterfaceClaseC
+ * 
+ * La interface InterfaceClaseC extiende las funcionalidades básicas de la interfaz 
+ * InterfaceRadio, añadiendo un método para cambiar el modo de operación del radio.
+ * 
+ * @author Adriana Martínez
+ * Fecha de creación: 14/11/2024
+ * Última modificación: 15/11/2024
+ */
 
 public interface InterfaceClaseC extends InterfaceRadio {
     /**
